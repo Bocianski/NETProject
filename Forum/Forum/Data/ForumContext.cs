5e6bc4525498fc45ae8751f8e7cc6934eeec0634
Bocianski <szyper212pl@gmail.com>
2024-05-09 10:11:47 +0200

    working migrations

---
+++ Forum/Forum/Data/ForumContext.cs
@@ -0,0 +1,11 @@
+﻿using Forum.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Forum.Data
+{
+    public class ForumContext: DbContext
+    {
+        public ForumContext(DbContextOptions<ForumContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Forum/Forum/Models/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace Forum.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string? PostTitle { get; set; }
+        public string? PostMessage { get; set; }
+        public DateTime? PostedDate { get; set; }
+    }
+}
